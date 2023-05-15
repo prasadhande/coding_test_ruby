e98a38c6787f5b727599d00150c4d42bcecbe57e
Prasad <prasadhande@gmail.com>
2023-05-15 13:58:21 +0530

    Update quadruplet_sum.rb

--- quadruplet_sum.rb
+++ quadruplet_sum.rb
@@ -1,27 +1,27 @@
-Quadruplet sums
+## Quadruplet sums
 
-Implement a function which finds four numbers within the provided list which have a sum equal to the target.
-
-
-An example is provided, but is too slow on larger testcases because it has too many nested loops.
+## Implement a function which finds four numbers within the provided list which have a sum equal to the target.
 
 
-
-There will always be at least one valid quadruplet.
-
-The same number can be used any number of times.
-
-Numbers can be returned in any order.
+## An example is provided, but is too slow on larger testcases because it has too many nested loops.
 
 
-Example
 
-numbers: [5, 4, 3, 2, 1, 0]
+## There will always be at least one valid quadruplet.
+
+## The same number can be used any number of times.
+
+## Numbers can be returned in any order.
 
 
-target: 11
+## Example
+
+## numbers: [5, 4, 3, 2, 1, 0]
 
 
-One possible answer is [1, 2, 3, 5]. Another possible answer is [5, 5, 1, 0].
+## target: 11
+
+
+## One possible answer is [1, 2, 3, 5]. Another possible answer is [5, 5, 1, 0].
 
 
