5ecfac241eca0f37f5b4c87cf181f1c040f5f7de
Prasad <prasadhande@gmail.com>
2023-05-15 12:58:38 +0530

    Update grand_father_books.rb

--- grand_father_books.rb
+++ grand_father_books.rb
@@ -1,1 +1,62 @@
+## The cat, the blind grandfather and the pile of books
+## I arranged my books in a nice pile, sorted by title.
+
+
+## Then, my cat bumped it, and the top of the pile fell. I was not able to see how many books were on the ground.
+
+
+## My blind grandfather couldn’t reorder them because he could not read the titles. However, he was kind enough to return the fallen books on top of the pile. The order was perfectly reversed, given how they fell and were put back.
+
+
+## For example, if I had this pile correctly sorted:
+
+
+
+## Band of Brothers
+
+## Frankenstein or The Modern Prometheus
+
+## Gone With the Wind
+
+## Harry Potter and the Prisoner of Azkaban
+
+## The Caves of Steel
+
+## The Grapes of Wrath
+
+## Ubik
+
+
+## Now, I have this one :
+
+
+
+## Harry Potter and the Prisoner of Azkaban
+
+## Gone With the Wind
+
+## Frankenstein or The Modern Prometheus
+
+## Band of Brothers
+
+## The Caves of Steel
+
+## The Grapes of Wrath
+
+## Ubik
+
+
+## How many books should I take and reverse at the top of the pile to get back my initial order?
+
+
+## You will get as input the list of strings containing the book titles in the final order. The first element is the top of the pile. With this example, it would be “Harry Potter and the Prisoner of Azkaban”.
+
+
+## The titles do not have any punctuation. I initially sorted them based on the entire string, case-insensitive. I do not remove article words like “The” or “A”. There are no duplicate titles.
+
+
+## With this example, the expected output would be 4 because I have to reverse the first 4 books to reorder my pile.
+
+
+## Bonus: try to code a solution that reads the fewest possible books in the input pile.
 
