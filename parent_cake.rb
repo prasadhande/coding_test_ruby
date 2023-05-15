50b946631400a155bd29e0140332d154cbe2e70f
Prasad <prasadhande@gmail.com>
2023-05-15 12:28:30 +0530

    Create parent_cake.rb

---
+++ parent_cake.rb
@@ -0,0 +1,30 @@
+## The most horrible parent in the world
+
+##  You have to share a cake with your two beloved children.
+
+
+##  Being the world’s worst parent, you want to split it into two random parts. Let’s say the cake is rectangular and 20cm long. You want to randomly choose a number between 0 and 20, defining where to cut the cake. For example, one of your children could have a 17cm part, whereas the other has only a 3cm part.
+
+
+## To choose the number, you only have a coin you can flip as many times as you want. So how would you make the most horrible cake split in the world?
+
+
+## Your solution must be able to choose all the possible numbers between 0 and 20 (included), but the distribution does not need to be perfectly uniform. Why would you care about such details as a horrible parent?
+
+
+## Bonus 1
+
+## Now with a cake of arbitrary size. It’s still rectangular. You get its size as an input parameter.
+
+
+## Bonus 2
+
+## You want to extend the process to anything you have to share with your children: orange juice, schoolbooks, money, … So you wish to produce a decimal number between 0 and 1 (included), but you still want to do it by flipping your coin.
+
+
+## Which additional information do you need?
+
+
+## Is there a limit? Would it depend on the computer and the programming language to implement the solution?
+
+
