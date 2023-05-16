e984eb6137a26e8eb071a4d659f43c173391db2f
Prasad <prasadhande@gmail.com>
2023-05-16 12:45:58 +0530

    Update fibonacci_sequence.rb

--- fibonacci_sequence.rb
+++ fibonacci_sequence.rb
@@ -1,29 +1,29 @@
 
-2048-bonacci
+## 2048-bonacci
 
-2048 is a tiny video game that was super hype some years ago. You had to fuse two equal numbers to create its double. So 1 and 1 became 2, 2 and 2 became 4, and so on.
-
-
-Fibonacci was a famous mathematician who made the super-hype Fibonacci sequence you may have heard about: U(n+1) = U(n) + U(n-1). The first number of the sequence are 1, 1, 2, 3, 5, 8, 13, …
+## 2048 is a tiny video game that was super hype some years ago. You had to fuse two equal numbers to create its double. So 1 and 1 became 2, 2 and 2 became 4, and so on.
 
 
-Let’s combine these two hype things to create a super-super-hype game!
+## Fibonacci was a famous mathematician who made the super-hype Fibonacci sequence you may have heard about: U(n+1) = U(n) + U(n-1). The first number of the sequence are 1, 1, 2, 3, 5, 8, 13, …
 
 
-2048-bonacci plays on a 4x4 square. Each square is either empty or contains a number of the Fibonacci sequence.
+## Let’s combine these two hype things to create a super-super-hype game!
 
 
-You are given an initial board situation (a 2D array of integers) and a pushing direction (up, left, down, or right). Then, you must compute the board contents after the push and return an updated 2D array of integers.
+## 2048-bonacci plays on a 4x4 square. Each square is either empty or contains a number of the Fibonacci sequence.
 
 
-The value of integers in the array is guaranteed to be less than 2^16 and are all Fibonacci numbers. The value 0 means the square is empty.
+## You are given an initial board situation (a 2D array of integers) and a pushing direction (up, left, down, or right). Then, you must compute the board contents after the push and return an updated 2D array of integers.
 
 
-Rule 1
-
-Numbers move as far as possible in the pushing direction.
+## The value of integers in the array is guaranteed to be less than 2^16 and are all Fibonacci numbers. The value 0 means the square is empty.
 
 
+## Rule 1
+
+## Numbers move as far as possible in the pushing direction.
+
+==begin
 -------------                       -------------
 | 2|  |  |  |                       |  |  |  |  |
 -------------                       -------------
@@ -83,7 +83,7 @@ Numbers can move to a square that a fusing has just emptied.
 -------------                     -------------
 
 Let’s hype!
-
+==end
 
 def fibonacci(n)
   return n if n <= 1
